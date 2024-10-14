6901c736b1c007121e0789987103671eadbe99e5
Mayi_a <devmayy@gmail.com>
2024-10-14 16:03:14 -0300

    Char qtd in string, 8, 12, 16.

--- geradorsenha.py
+++ geradorsenha.py
@@ -1,10 +1,29 @@
 import random
+from random import randint
 import string
+
 
 digits = string.ascii_lowercase + string.ascii_uppercase + string.digits
 simb = ['@','!','$']
 
-passwordRandom = (''.join(random.sample(digits, 9)))
+position = randint(4, 16)
+passwordRandom = (''.join(random.sample(digits, position)))
 
 
-print (''.join('%s%s' % (x,random.choice(simb) if random.random() > 0.8 else '') for x in passwordRandom))
+passwordComplete = (''.join('%s%s' % (x,random.choice(simb) if random.random() > 0.8 else '') for x in passwordRandom))
+
+
+def passwordLevels():
+    qtd = len(passwordComplete)
+
+    if qtd < 8:
+        return "low"
+    elif qtd < 12:
+        return "medium"
+    else: qtd < 16 
+    return "high"  
+
+level = passwordLevels()
+
+print(passwordComplete)
+print(f'Password level: {level} ')
