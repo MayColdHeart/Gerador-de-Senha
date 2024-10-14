1073a87598fa7713721284a0e114686c0fdaa096
Mayi_a <devmayy@gmail.com>
2024-10-14 15:38:12 -0300

    Add simbol in random points

--- geradorsenha.py
+++ geradorsenha.py
@@ -2,6 +2,9 @@ import random
 import string
 
 digits = string.ascii_lowercase + string.ascii_uppercase + string.digits
+simb = ['@','!','$']
+
+passwordRandom = (''.join(random.sample(digits, 9)))
 
 
-print(''.join(random.sample(digits, 16)))
+print (''.join('%s%s' % (x,random.choice(simb) if random.random() > 0.8 else '') for x in passwordRandom))
