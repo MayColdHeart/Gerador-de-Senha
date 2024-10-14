237343b73cb0afd88d40ca56f0c03c8292676d51
Mayi_a <devmayy@gmail.com>
2024-10-14 15:28:54 -0300

    update function

--- geradorsenha.py
+++ geradorsenha.py
@@ -1,17 +1,7 @@
 import random
 import string
-string.ascii_letters
-'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
-random.seed 
+digits = string.ascii_lowercase + string.ascii_uppercase + string.digits
 
-combinacao_1 = random.randint(0, 100)
-combinacao_2 = random.randrange(101, 150)
-combinacao_3 = random.randint(200, 300)
-combinacao_4 = random.randint(400, 540)
-combinacao_5 = random.choice(string.ascii_letters)
-combinacao_6 = random.choice(string.ascii_letters)
-combinacao_7 = random.choice(string.ascii_letters)
 
-print("\n")
-print(f'Senha adquirida: {combinacao_2}{combinacao_6}{combinacao_7}{combinacao_4}{combinacao_3}{combinacao_1}{combinacao_5}')
+print(''.join(random.sample(digits, 16)))
