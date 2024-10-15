4187626cdfabb6facbd899e504fceb49785f08ca
Mayi_a <devmayy@gmail.com>
2024-10-14 22:22:57 -0300

    Name in password update

--- geradorsenha.py
+++ geradorsenha.py
@@ -2,15 +2,17 @@ import random
 from random import randint
 import string
 
+text = input('any name: ')
 
 digits = string.ascii_lowercase + string.ascii_uppercase + string.digits
 simb = ['@','!','$']
 
-position = randint(4, 16)
+position = randint(4, 12)
 passwordRandom = (''.join(random.sample(digits, position)))
 
 
-passwordComplete = (''.join('%s%s' % (x,random.choice(simb) if random.random() > 0.8 else '') for x in passwordRandom))
+passwordPrimary = (''.join('%s%s' % (x,random.choice(simb) if random.random() > 0.5 else '') for x in passwordRandom))
+passwordComplete = (''.join('%s%s' % (y, random.choice(text) if random.random()> 0.8 else '') for y in passwordPrimary))
 
 
 def passwordLevels():
